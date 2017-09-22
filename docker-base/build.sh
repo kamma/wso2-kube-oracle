cade0dc4697ada37ac8acba5971eb8a745155d39
kAmMa <kamma@shrt.cz>
2017-09-22 11:11:54 +0200

    fixed build files

--- docker-base/build.sh
+++ docker-base/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-docker build -t rsync:1.0.0 rsync
-docker build -t sshd:1.0.0 sshd
-docker build -t wso2am:2.1.0 apim
-docker build -t wso2am-analytics:2.1.0 analytics
+this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+rsync_dir=$(cd "${this_dir}/rsync"; pwd)
+sshd_dir=$(cd "${this_dir}/sshd"; pwd)
+analytics_dir=$(cd "${this_dir}/analytics"; pwd)
+apim_dir=$(cd "${this_dir}/apim"; pwd)
 
+docker build -t rsync:1.0.0 $rsync_dir
+docker build -t sshd:1.0.0 $sshd_dir
+docker build -t wso2am:2.1.0 $apim_dir
+docker build -t wso2am-analytics:2.1.0 $analytics_dir
--- docker-conf/build.sh
+++ docker-conf/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker build -t wso2am-analytics-1-pattern-1:2.1.0 apim_analytics_1
-docker build -t wso2am-analytics-2-pattern-1:2.1.0 apim_analytics_2
-docker build -t wso2am-manager-worker-pattern-1:2.1.0 apim_manager_worker
-docker build -t wso2am-worker-pattern-1:2.1.0 apim_worker
+this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+apim_analytics_1_dir=$(cd "${this_dir}/apim-analytics-1"; pwd)
+apim_analytics_2_dir=$(cd "${this_dir}/apim-analytics-2"; pwd)
+apim_manager_worker_dir=$(cd "${this_dir}/apim-manager-worker"; pwd)
+apim_worker_dir=$(cd "${this_dir}/apim-worker"; pwd)
+
+docker build -t wso2am-analytics-1-pattern-1:2.1.0 $apim_analytics_1_dir
+docker build -t wso2am-analytics-2-pattern-1:2.1.0 $apim_analytics_2_dir
+docker build -t wso2am-manager-worker-pattern-1:2.1.0 $apim_manager_worker_dir
+docker build -t wso2am-worker-pattern-1:2.1.0 $apim_worker_dir
