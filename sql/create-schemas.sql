3252233b0ce6cdcc01a382f525747d056c890679
kAmMa <kamma@shrt.cz>
2017-09-22 16:44:05 +0200

    added connections count in datasource configs

--- sql/create-schemas.sql
+++ sql/create-schemas.sql
@@ -1,10 +1,7 @@
 drop user apimgtdb cascade;
 drop user govregdb cascade;
 drop user userdb cascade;
-drop user mbstoredb cascade;
 drop user statdb cascade;
-drop user analyticseventstoredb cascade;
-drop user analyticprocesseddatastoredb cascade;
 
 /************************************* apimgtdb *********************************/
 create user apimgtdb identified by Passw0rd account unlock DEFAULT TABLESPACE "USERS" TEMPORARY TABLESPACE "TEMP";
@@ -21,23 +18,8 @@ create user userdb identified by Passw0rd account unlock DEFAULT TABLESPACE "USE
 grant connect to userdb;
 grant create session, create table, create sequence, create trigger to userdb;
 alter user userdb quota UNLIMITED on USERS;
-/************************************* mbstoredb  ********/
-create user mbstoredb identified by Passw0rd account unlock DEFAULT TABLESPACE "USERS" TEMPORARY TABLESPACE "TEMP";
-grant connect to mbstoredb;
-grant create session, create table, create sequence, create trigger to mbstoredb;
-alter user mbstoredb quota UNLIMITED on USERS;
 /************************************* statdb *********************************/
 create user statdb identified by Passw0rd account unlock DEFAULT TABLESPACE "USERS" TEMPORARY TABLESPACE "TEMP";
 grant connect to statdb;
 grant create session, create table, create sequence, create trigger to statdb;
 alter user statdb quota UNLIMITED on USERS;
-/************************************* analyticseventstoredb *********************************/
-create user analyticseventstoredb identified by Passw0rd account unlock DEFAULT TABLESPACE "USERS" TEMPORARY TABLESPACE "TEMP";
-grant connect to analyticseventstoredb;
-grant create session, create table, create sequence, create trigger to analyticseventstoredb;
-alter user analyticseventstoredb quota UNLIMITED on USERS;
-/************************************* statdb *********************************/
-create user analyticprocesseddatastoredb identified by Passw0rd account unlock DEFAULT TABLESPACE "USERS" TEMPORARY TABLESPACE "TEMP";
-grant connect to analyticprocesseddatastoredb;
-grant create session, create table, create sequence, create trigger to analyticprocesseddatastoredb;
-alter user analyticprocesseddatastoredb quota UNLIMITED on USERS;
