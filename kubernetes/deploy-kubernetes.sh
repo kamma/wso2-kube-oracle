8fad8e39fbe0a9986c2680083557cceae1b1f5a4
kAmMa <kamma@shrt.cz>
2017-09-22 11:30:38 +0200

    removed mysql rdbms

--- kubernetes/deploy-kubernetes.sh
+++ kubernetes/deploy-kubernetes.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
-# service account
+echo 'creating service account ...'
 kubectl create serviceaccount wso2svcacct
 
-# volume
-echo 'deploying volume ...'
+echo 'creating persistent volumes ...'
 kubectl apply -f volume/persistent-volumes.yaml
 
-# databases
-echo 'deploying databases ...'
-kubectl apply -f rdbms/rdbms-persistent-volume-claim.yaml
-kubectl apply -f rdbms/rdbms-service.yaml
-kubectl apply -f rdbms/rdbms-deployment.yaml
-
-sleep 30s
-# analytics
-echo 'deploying apim analytics ...'
+echo 'creating services and volume claims ...'
 kubectl apply -f apim-analytics/wso2apim-analytics-service.yaml
 kubectl apply -f apim-analytics/wso2apim-analytics-1-service.yaml
 kubectl apply -f apim-analytics/wso2apim-analytics-2-service.yaml
-kubectl apply -f apim-analytics/wso2apim-analytics-1-deployment.yaml
-sleep 10s
-kubectl apply -f apim-analytics/wso2apim-analytics-2-deployment.yaml
 
-sleep 1m
-# apim
 kubectl apply -f apim/wso2apim-mgt-volume-claim.yaml
 kubectl apply -f apim/wso2apim-worker-volume-claim.yaml
 kubectl apply -f apim/wso2apim-service.yaml
 kubectl apply -f apim/wso2apim-manager-worker-service.yaml
 kubectl apply -f apim/wso2apim-worker-service.yaml
+
+
+
+echo 'deploying apim analytics-1 ...'
+kubectl apply -f apim-analytics/wso2apim-analytics-1-deployment.yaml
+sleep 1m
+echo 'deploying apim analytics-2 ...'
+kubectl apply -f apim-analytics/wso2apim-analytics-2-deployment.yaml
+sleep 1m
 echo 'deploying apim manager-worker ...'
 kubectl apply -f apim/wso2apim-manager-worker-deployment.yaml
 sleep 1m
