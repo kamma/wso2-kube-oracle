45839cd16a5620d01dff779f06e48877bac6362f
kAmMa <kamma@shrt.cz>
2017-09-22 16:31:31 +0200

    changed deploy script

--- kubernetes/deploy-kubernetes.sh
+++ kubernetes/deploy-kubernetes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo 'creating service account ...'
+echo '-- creating service account ...'
 kubectl create serviceaccount wso2svcacct
 
-echo 'creating persistent volumes ...'
+echo '-- creating persistent volumes ...'
 kubectl apply -f volume/persistent-volumes.yaml
 
-echo 'creating services and volume claims ...'
+echo '-- creating services and volume claims ...'
 kubectl apply -f apim-analytics/wso2apim-analytics-service.yaml
 kubectl apply -f apim-analytics/wso2apim-analytics-1-service.yaml
 kubectl apply -f apim-analytics/wso2apim-analytics-2-service.yaml
@@ -17,16 +17,17 @@ kubectl apply -f apim/wso2apim-service.yaml
 kubectl apply -f apim/wso2apim-manager-worker-service.yaml
 kubectl apply -f apim/wso2apim-worker-service.yaml
 
-
-
-echo 'deploying apim analytics-1 ...'
+echo '-- deploying apim analytics-1 ...'
 kubectl apply -f apim-analytics/wso2apim-analytics-1-deployment.yaml
+echo '-- waiting for 1 minute'
 sleep 1m
-echo 'deploying apim analytics-2 ...'
+echo '-- deploying apim analytics-2 ...'
 kubectl apply -f apim-analytics/wso2apim-analytics-2-deployment.yaml
+echo '-- waiting for 1 minute'
 sleep 1m
-echo 'deploying apim manager-worker ...'
+echo '-- deploying apim manager-worker ...'
 kubectl apply -f apim/wso2apim-manager-worker-deployment.yaml
+echo '-- waiting for 1 minute'
 sleep 1m
-echo 'deploying apim worker ...'
+echo '-- deploying apim worker ...'
 kubectl apply -f apim/wso2apim-worker-deployment.yaml
