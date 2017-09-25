e4fcf9b6e992695aa6ee9208e52393acca311f1e
kAmMa <kamma@shrt.cz>
2017-09-25 09:35:18 +0200

    added key manager profile

--- docker-conf/build.sh
+++ docker-conf/build.sh
@@ -5,8 +5,10 @@ apim_analytics_1_dir=$(cd "${this_dir}/apim-analytics-1"; pwd)
 apim_analytics_2_dir=$(cd "${this_dir}/apim-analytics-2"; pwd)
 apim_manager_worker_dir=$(cd "${this_dir}/apim-manager-worker"; pwd)
 apim_worker_dir=$(cd "${this_dir}/apim-worker"; pwd)
+apim_keyman_dir=$(cd "${this_dir}/apim-keyman"; pwd)
 
 docker build -t wso2am-analytics-1-pattern-1:2.1.0 $apim_analytics_1_dir
 docker build -t wso2am-analytics-2-pattern-1:2.1.0 $apim_analytics_2_dir
 docker build -t wso2am-manager-worker-pattern-1:2.1.0 $apim_manager_worker_dir
 docker build -t wso2am-worker-pattern-1:2.1.0 $apim_worker_dir
+docker build -t wso2am-keyman-pattern-1:2.1.0 $apim_keyman_dir
