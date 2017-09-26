1d88875cac2f993291accd32553836507bf5da34
kAmMa <kamma@shrt.cz>
2017-09-26 18:28:07 +0200

    initial commit for pattern-2

--- docker-base/sshd/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo 'generating new server keys with ssh-keygen -A'
-echo 'ssh-keygen -A'
-### useful for debugging ssh related issues
-#rsyslogd
-###
-echo 'starting sshd deamon'
-/usr/sbin/sshd -D && tailf /var/log/auth.log 2>&1 
