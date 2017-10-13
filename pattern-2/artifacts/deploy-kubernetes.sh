ca1943681a040b1077a9b6be6f391de6262635c9
kAmMa <kamma@shrt.cz>
2017-10-13 10:19:03 +0200

    move datasource replacement to anothe script

--- pattern-2/artifacts/deploy-kubernetes.sh
+++ pattern-2/artifacts/deploy-kubernetes.sh
@@ -16,25 +16,6 @@
 # limitations under the License
 # ------------------------------------------------------------------------
 
-EQUA_DS=excdb01-vip-devl.equabank.loc:1523/D1GFO.equabank.loc
-EQUA_PWD=oracle1
-LOCAL_DS=10.201.152.1:49161/xe
-LOCAL_PWD=Password
-
-case $1 in
-equa)
-        find ../confs -type f -name *.xml -exec sed -i "s|<JDBC_DATASOURCE>|$EQUA_DS|g" {} +
-        find ../confs -type f -name *.xml -exec sed -i "s|<JDBC_PASSWORD>|$EQUA_PWD|g" {} +
-;;
-local)
-        find ../confs -type f -name *.xml -exec sed -i "s|<JDBC_DATASOURCE>|$LOCAL_DS|g" {} +
-        find ../confs -type f -name *.xml -exec sed -i "s|<JDBC_PASSWORD>|$LOCAL_PWD|g" {} +
-;;
-*)
-        echo don\'t know
-;;
-esac
-
 kubectl create namespace wso2
 
 # set namespace
