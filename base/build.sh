f56c7dae4de4d25dea93cae746b996ccb329d977
kAmMa <kamma@shrt.cz>
2017-10-13 09:03:51 +0200

    rewritten docker build - more simple

--- base/build.sh
+++ base/build.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# builds the base images - apim, analytics
-
-set -e
-
-this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-analytics_dir=$(cd "${this_dir}/analytics"; pwd)
-apim_dir=$(cd "${this_dir}/apim"; pwd)
-ids_dir=$(cd "${this_dir}/ids"; pwd)
-
-function docker_build() {
-    tag=$1
-    path=$2
-    docker_api_version=`docker version | grep -m2 "API version" | tail -n1 | cut -d' ' -f5 | bc -l`
-    echo "Building Docker image ${tag}..."
-    if (( $(echo ${docker_api_version} '>=' 1.25 | bc -l) )); then
-        docker build -t ${tag} ${path} --squash
-    else
-        echo "Docker API version is ${docker_api_version}, ignoring --squash option"
-        docker build -t ${tag} ${path}
-    fi
-}
-
-docker_build wso2am-kubernetes:2.1.0 $apim_dir
-docker_build wso2am-analytics-kubernetes:2.1.0 $analytics_dir
-docker_build wso2am-km-kubernetes:2.1.0 $ids_dir
+docker build -t wso2am-kubernetes:2.1.0 ./apim
+docker build -t wso2am-analytics-kubernetes:2.1.0 ./analytics
+docker build -t wso2am-km-kubernetes:2.1.0 ./ids
