3bc39c94127777d0c8e5099756bbc1065cadf3bb
kAmMa <kamma@shrt.cz>
2017-11-24 16:06:08 +0100

    added open_cursors to SQL

--- sql/alter-connections.sql
+++ sql/alter-connections.sql
@@ -1,3 +1,4 @@
 alter system set processes = 1000 scope = spfile;
 alter system set sessions = 1000 scope = spfile;
 alter system set transactions = 1000 scope = spfile;
+alter system set open_cursors = 1000 scope = spfile;
