d661340e3dbb133f07f43e55ea9a945c0783ccc7
kAmMa <kamma@shrt.cz>
2017-12-04 13:50:51 +0100

    changed docker build script

--- base/build.sh
+++ base/build.sh
@@ -4,24 +4,9 @@
 
 set -e
 
-this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-analytics_dir=$(cd "${this_dir}/analytics"; pwd)
-apim_dir=$(cd "${this_dir}/apim"; pwd)
-ids_dir=$(cd "${this_dir}/ids"; pwd)
+TAG_NAME=$1
 
-function docker_build() {
-    tag=$1
-    path=$2
-    docker_api_version=`docker version | grep -m2 "API version" | tail -n1 | cut -d' ' -f5 | bc -l`
-    echo "Building Docker image ${tag}..."
-    if (( $(echo ${docker_api_version} '>=' 1.25 | bc -l) )); then
-        docker build -t ${tag} ${path}
-    else
-        echo "Docker API version is ${docker_api_version}, ignoring --squash option"
-        docker build -t ${tag} ${path}
-    fi
-}
-
-docker_build wso2am-kubernetes:2.1.0 $apim_dir
-docker_build wso2am-analytics-kubernetes:2.1.0 $analytics_dir
-docker_build wso2am-km-kubernetes:2.1.0 $ids_dir
+echo "Building docker images for APIM with tag: $TAG_NAME"
+docker build -t wso2am-kubernetes:$TAG_NAME ./apim
+docker build -t wso2am-analytics-kubernetes:$TAG_NAME ./analytics
+docker build -t wso2am-km-kubernetes:$TAG_NAME ./ids
